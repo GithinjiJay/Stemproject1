ba240f3f0e69fe474187f67388f1cbea61100d54
Jay <githinjijay388@gmail.com>
2024-02-20 20:33:21 +0300

    Initial commit

---
+++ welcome.py
@@ -0,0 +1,25 @@
+print("My name is Jay")
+greating = "Hello"
+name = "Jay"
+age = 30
+location = "Juja"
+print(name)
+print(age)
+school = "Jkuat"
+print(school)
+height = 2.3 
+print(height)
+first_name = "Jayz"
+last_name = "Doe"
+name = 30
+x = 40
+y = 4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+x,y,z,u=10,4,6,9
+print(x)
+first_name = input("Enter name: ")
+last_name = input("Enter your last name: ")
+print(greating + first_name + last_name)
