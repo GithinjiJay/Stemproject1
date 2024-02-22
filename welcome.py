995174811bba2515b6caa0015dfbecfde5936e77
Jay <githinjijay388@gmail.com>
2024-02-22 12:20:26 +0300

    day 2

--- welcome.py
+++ welcome.py
@@ -1,27 +1,51 @@
-print("My name is Jay")
-greating = "Hello"
-name = "Jay"
-age = 30
-location = "Juja"
-print(name)
-print(age)
-school = "Jkuat"
-print(school)
-height = 2.3 
-print(height)
-first_name = "Jayz"
-last_name = "Doe"
-name = 30
-x = 40
-y = 4
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)
-x,y,z,u=10,4,6,9
-print(x)
-first_name = input("Enter name: ")
-last_name = input("Enter your Last Name: ")
-print(greating + first_name + last_name)
-year = 2024
-print(year)
+#print("My name is Jay")
+#greating = "Hello"
+#name = "Jay"
+#age = 30
+#location = "Juja"
+#print(name)
+#print(age)
+#school = "Jkuat"
+#print(school)
+#height = 2.3 
+#print(height)
+#first_name = "Jayz"
+#last_name = "Doe"
+#name = 30
+#x = 40
+#y = 4
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#x,y,z,u=10,4,6,9
+#print(x)
+#first_name = input("Enter name: ")
+#last_name = input("Enter your Last Name: ")
+#print(greating + first_name + last_name)
+#firstname = "Joe"
+
+firstname = "Joel"
+secondname = "Njau"
+print(firstname + secondname)
+firstnumber = 7
+secondnumber = 30
+sum = firstnumber - secondnumber
+print(sum)
+pi = 22/7
+radius = 7 
+sum = pi * radius * radius
+print(sum)
+a = 3
+b = 7 
+c = 2
+area = 1/2 * (a + b) * c
+print(area)
+print("The area is",area)
+name = input("enter first name")
+print (name)
+r = int(input("enter the radius"))
+pi = 22/7
+area = pi * r * r
+print (area)
+
